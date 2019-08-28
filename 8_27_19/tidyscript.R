1aad69634b6684e21457ace88aff477f66666965
katie hannibal <katherine.hannibal.34@my.csun.edu>
2019-08-27 18:04:46 -0700

    New info

--- 8_27_19/tidyscript.R
+++ 8_27_19/tidyscript.R
@@ -3,3 +3,5 @@
 #commit: timestamp, goes back in time
 #push: puts online
 #pull: takes offline onto r studio
+#Online things will disappear from the right side as they go online-->
+#M for modified means that it is new and saved but not comitted or pushed
