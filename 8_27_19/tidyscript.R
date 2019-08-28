9e5966d17da79b5b3202c4e8b7b08fe55123bf3f
katie hannibal <katherine.hannibal.34@my.csun.edu>
2019-08-27 18:16:08 -0700

    Simpsons Data

--- 8_27_19/tidyscript.R
+++ 8_27_19/tidyscript.R
@@ -6,3 +6,8 @@
 #Online things will disappear from the right side as they go online-->
 #M for modified means that it is new and saved but not comitted or pushed
 #trying to pull
+#starting simpsons
+install.packages('tidyverse') #installing tidyverse
+library('tidyverse') #load library
+simpsons <- readr::read_delim("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-27/simpsons-guests.csv", delim = "|", quote = "")
+
