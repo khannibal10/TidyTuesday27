96f305b2ed5b70544d446506351a2f8ce2647427
khannibal10 <54605514+khannibal10@users.noreply.github.com>
2019-08-27 18:06:27 -0700

    Update tidyscript.R

--- 8_27_19/tidyscript.R
+++ 8_27_19/tidyscript.R
@@ -5,3 +5,4 @@
 #pull: takes offline onto r studio
 #Online things will disappear from the right side as they go online-->
 #M for modified means that it is new and saved but not comitted or pushed
+#trying to pull
